987c43b7c7ad116a587748caf5086ecab9a4c2ed
kumarvastav <kumar.vastav@gmail.com>
2016-07-18 20:12:45 +0530

    Added sample Baseline spec and form

---
+++ specs/endtb/RecordEnrolledPatientsObservations.spec
@@ -0,0 +1,25 @@
+Record Observations for Enrolled Patient
+========================================
+Created by dharmens on 7/18/16
+
+This is an executable specification file which follows markdown syntax.
+Every heading in this file denotes a scenario. Every bulleted point denotes a step.
+     
+Fill Observations for enrolled patient
+--------------------------------------
+Tags: Smoke, Sanity
+
+* Create a random patient
+* Click on treatment enrollment
+* Register the patient to following program
+    |TB Register|Date Of Registration|Registration Number|Registration Facility|
+    |Basic management unit TB register|01/01/2015|ABC|Facility1, City1, Country1|
+
+//* Click on Treatment dashboard
+//* Navigate to Observation
+//* Add observation template <templateName>
+//* Fill the observation template
+//    |Date Baseline Assessment|Marital Status|Homeless|Employed|Has ThePatientEverBeenTreatedForTBInThePast|
+//    |06/19/2016|Married|No|Yes|Yes|
+//
+//* Validate the observations are shown on the dashboard
--- src/main/java/org/bahmni/endtb/page/clinical/BaselinePage.java
+++ src/main/java/org/bahmni/endtb/page/clinical/BaselinePage.java
@@ -17,5 +17,41 @@ public class BaselinePage extends BahmniPage {
 	@FindBy(how= How.XPATH, using ="//div[starts-with(@id,'concept-set-')]")
     public List<WebElement> concept_text;
 
-    
+    @FindBy(how= How.CSS, using =".control-label>span[ng-bind='::observation.label']")
+    public List<WebElement> alllabel;
+
+    public void fillTemplateData(String labelname,String labeltype,String labelvalue){
+        for(int j=0;j<alllabel.size();j++){
+            if(alllabel.get(j).getText().equals(labelname)){
+
+                if(labeltype.equals("textbox"))
+                {
+                    alllabel.get(j).findElement(By.xpath("../../div/div/div/input")).sendKeys(labelvalue);
+                }
+                else if(labeltype.equals("button"))
+                {
+                    List<WebElement> allbutton=alllabel.get(j).findElements(By.xpath("../../div/div/div/div/div/button-select/div/button"));
+                    for(int i=0;i<allbutton.size();i++){
+                        if(allbutton.get(i).getText().equals(labelvalue)){
+                            allbutton.get(i).click();
+                            break;
+                        }
+
+                    }
+                }
+                else if(labeltype.equals("textarea"))
+                {
+                    alllabel.get(j).findElement(By.xpath("../../div/div/div/textarea")).sendKeys(labelvalue);
+                }
+
+            }
+
+        }
+
+    }
+
+
+    public void fillBaselineTemplate(){
+
+    }
 }
