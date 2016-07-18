9cec2912f2f6b3517aa5657c74c692aff715c5ab
swarupkm <swarupmahapatra1@gmail.com>
2016-07-18 12:38:33 +0530

    Added amman folder

---
+++ specs/amman/createPatientAndSearch.spec
@@ -0,0 +1,7 @@
+Create Amamn Patient Scenarios
+===============================
+
+CREATE PATIENT AND VERIFY
+-------------------------
+*step 1
+*step 2
---
+++ src/test/java/org/bahmni/amman/page/CreatePatientSpec.java
@@ -0,0 +1,16 @@
+package org.bahmni.amman.page;
+import com.thoughtworks.gauge.Step;
+/**
+ * Created by swarup on 18/7/16.
+ */
+public class CreatePatientSpec {
+    @Step("step 1")
+    public void implementation1() {
+
+    }
+
+    @Step("step 2")
+    public void implementation2() {
+
+    }
+}
