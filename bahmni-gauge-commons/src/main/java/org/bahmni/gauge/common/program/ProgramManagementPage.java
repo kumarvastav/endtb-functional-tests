94bbef9f1522bdf512fd8b5ab487d8fd754c7339
Buddha <sidthunagarajan@gmail.com>
2016-08-31 18:06:29 +0530

    Pankaj, Gautam | Added wait condition to wait till delete program popup is vissible. Tagged autoenrolment spec with regression @ spec level.

--- bahmni-gauge-commons/src/main/java/org/bahmni/gauge/common/program/ProgramManagementPage.java
+++ bahmni-gauge-commons/src/main/java/org/bahmni/gauge/common/program/ProgramManagementPage.java
@@ -10,161 +10,169 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.Random;
 
 public class ProgramManagementPage extends BahmniPage {
 
-	@FindBy(how = How.CSS, using = "select.ng-pristine")
-	public WebElement program;
+    @FindBy(how = How.CSS, using = "select.ng-pristine")
+    public WebElement program;
 
-	@FindBy(how = How.CSS, using = ".fa-plus-square")
-	public WebElement btnPlus;
+    @FindBy(how = How.CSS, using = ".fa-plus-square")
+    public WebElement btnPlus;
 
-	@FindBy(how = How.CSS, using = "input.ng-valid-max")
-	public WebElement start_date;
+    @FindBy(how = How.CSS, using = "input.ng-valid-max")
+    public WebElement start_date;
 
-	@FindBy(how = How.XPATH, using = "//input[@value='Enroll' and @type='submit']")
-	public WebElement btnEnroll;
+    @FindBy(how = How.XPATH, using = "//input[@value='Enroll' and @type='submit']")
+    public WebElement btnEnroll;
 
-	@FindBy(how = How.CSS, using = ".active-program-tiles")
-	public List<WebElement> allActivePrograms;
+    @FindBy(how = How.CSS, using = ".active-program-tiles")
+    public List<WebElement> allActivePrograms;
 
-	@FindBy(how = How.CSS, using = ".inactive-program-tiles")
-	public List<WebElement> inactive_progs;
+    @FindBy(how = How.CSS, using = ".inactive-program-tiles")
+    public List<WebElement> inactive_progs;
 
-	@FindBy(how = How.CSS, using = "[ng-model='patientProgram.outcomeData']")
-	public WebElement treatment_status;
+    @FindBy(how = How.CSS, using = "[ng-model='patientProgram.outcomeData']")
+    public WebElement treatment_status;
 
-	@FindBy(how = How.CSS, using = "#dashboard-link")
-	public List<WebElement> treatment_dashboard;
+    @FindBy(how = How.CSS, using = "#dashboard-link")
+    public List<WebElement> treatment_dashboard;
 
-	@FindBy(how = How.CSS, using = ".active-program-container")
-	public WebElement activeProgramContainer;
+    @FindBy(how = How.CSS, using = ".active-program-container")
+    public WebElement activeProgramContainer;
 
-	@FindBy(how= How.CSS, using = "[id='Facility Name']")
-	public WebElement facility_name;
+    @FindBy(how = How.CSS, using = "[id='Facility Name']")
+    public WebElement facility_name;
 
-	@FindBy(how= How.CSS, using = "[id='Sample attribute3']")
-	public WebElement facility_id;
+    @FindBy(how = How.CSS, using = "[id='Sample attribute3']")
+    public WebElement facility_id;
 
-	@FindBy(how= How.CSS, using = "#Doctor")
-	public WebElement doctor;
+    @FindBy(how = How.CSS, using = "#Doctor")
+    public WebElement doctor;
 
-	@FindBy(how= How.CSS, using = "#Id_Number")
-	public WebElement registration_id;
+    @FindBy(how = How.CSS, using = "#Id_Number")
+    public WebElement registration_id;
 
-	@FindBy(how= How.CSS, using = ".delete-program-popup")
-	public WebElement deleteProgramPopUp;
+    public ProgramManagementPage enrollPatientToProgram(Program treatment) {
+        doActions(treatment);
+        btnEnroll.click();
+        return this;
+    }
 
+    protected void doActions(Program treatment) {
+        btnPlus.click();
+        new Select(program).selectByVisibleText(treatment.getName());
+        start_date.sendKeys(treatment.getDateOfRegistration());
+    }
 
-	public ProgramManagementPage enrollPatientToProgram(Program treatment) {
-		doActions(treatment);
-		btnEnroll.click();
-		return this;
-	}
+    public void modifyProgramEnrollment(Program treatment) {
 
-	protected void doActions(Program treatment) {
-		btnPlus.click();
-		new Select(program).selectByVisibleText(treatment.getName());
-		start_date.sendKeys(treatment.getDateOfRegistration());
-	}
+        WebElement programToModify = findProgram(treatment);
+        programToModify.findElement(By.cssSelector("[value='Edit']")).click();
+        doModifyEnrollmentDetails(programToModify, treatment);
+        programToModify.findElement(By.cssSelector("[value='Save']")).click();
+    }
 
-	public void modifyProgramEnrollment(Program treatment) {
+    protected void doModifyEnrollmentDetails(WebElement programToModify, Program treatment) {
 
-		WebElement programToModify = findProgram(treatment);
-		programToModify.findElement(By.cssSelector("[value='Edit']")).click();
-		doModifyEnrollmentDetails(programToModify, treatment);
-		programToModify.findElement(By.cssSelector("[value='Save']")).click();
-	}
+        if (treatment.getTreatmentStatus() != null) {
+            WebElement outcome = programToModify.findElement(By.cssSelector("[ng-model='patientProgram.outcomeData']"));
+            new Select(outcome).selectByVisibleText(treatment.getTreatmentStatus());
+        }
 
-	protected void doModifyEnrollmentDetails(WebElement programToModify, Program treatment) {
+        //TODO: Write modification for DateOfRegistration
+    }
 
-		if (treatment.getTreatmentStatus() != null) {
-			WebElement outcome = programToModify.findElement(By.cssSelector("[ng-model='patientProgram.outcomeData']"));
-			new Select(outcome).selectByVisibleText(treatment.getTreatmentStatus());
-		}
+    private WebElement findProgram(Program program) {
+        return findProgram(program.getName());
+    }
 
-		//TODO: Write modification for DateOfRegistration
-	}
+    public WebElement findProgram(String programName) {
+        for (WebElement allActiveProgram : allActivePrograms) {
+            if (allActiveProgram.getText().contains(programName)) {
+                return allActiveProgram;
+            }
+        }
+        throw new TestSpecException("The program with name [" + programName + "] doesn't exist");
+    }
 
-	private WebElement findProgram(Program program) {
-		return findProgram(program.getName());
-	}
-	public WebElement findProgram(String programName) {
-		for (WebElement allActiveProgram : allActivePrograms) {
-			if (allActiveProgram.getText().contains(programName)) {
-				return allActiveProgram;
-			}
-		}
-		throw new TestSpecException("The program with name [" + programName + "] doesn't exist");
-	}
+    public void clickTreatmentDashboard(PatientProgram patientProgram) {
+        clickTreatmentDashboard(patientProgram.getProgram());
+    }
 
-	public void clickTreatmentDashboard(PatientProgram patientProgram) {
-		clickTreatmentDashboard(patientProgram.getProgram());
-	}
-	public void clickTreatmentDashboard(Program program){
-		WebElement programWidget = findProgram(program);
-		programWidget.findElement(By.id("dashboard-link")).click();
+    public void clickTreatmentDashboard(Program program) {
+        WebElement programWidget = findProgram(program);
+        programWidget.findElement(By.id("dashboard-link")).click();
 
-	}
-	public boolean isPatientEnrolledToProgram(Program treatment) {
-		WebElement programName = activeProgramContainer.findElement(By.cssSelector(".programName"));
-		return isProgramAvailable(treatment, programName);
-	}
+    }
 
-	private boolean isProgramAvailable(Program enrolledProgram, WebElement programName) {
-		return programName != null && programName.getText().equals(enrolledProgram.getName());
-	}
+    public boolean isPatientEnrolledToProgram(Program treatment) {
+        WebElement programName = activeProgramContainer.findElement(By.cssSelector(".programName"));
+        return isProgramAvailable(treatment, programName);
+    }
 
-	public Program transformTableToProgram(Table table) {
-		List<TableRow> rows = table.getTableRows();
-		List<String> columnNames = table.getColumnNames();
+    private boolean isProgramAvailable(Program enrolledProgram, WebElement programName) {
+        return programName != null && programName.getText().equals(enrolledProgram.getName());
+    }
 
-		if (rows.size() != 1) {
-			throw new TestSpecException("Only one patient should be provided in the table");
-		}
+    public Program transformTableToProgram(Table table) {
+        List<TableRow> rows = table.getTableRows();
+        List<String> columnNames = table.getColumnNames();
 
-		String programName = rows.get(0).getCell(columnNames.get(0));
-		String dateOfRegistration = rows.get(0).getCell(columnNames.get(1));
+        if (rows.size() != 1) {
+            throw new TestSpecException("Only one patient should be provided in the table");
+        }
 
-		return new Program(programName,dateOfRegistration);
-	}
+        String programName = rows.get(0).getCell(columnNames.get(0));
+        String dateOfRegistration = rows.get(0).getCell(columnNames.get(1));
 
-	public void editProgramAttributes(Program treatment, String registration, String facility) {
-		WebElement programToModify = findProgram(treatment);
-		programToModify.findElement(By.cssSelector("[value='Edit']")).click();
-		editProgram(registration, facility);
-		programToModify.findElement(By.cssSelector("[value='Save']")).click();
-	}
+        return new Program(programName, dateOfRegistration);
+    }
 
-	public void endProgram(Program treatment) {
-		WebElement programToModify = findProgram(treatment);
-		programToModify.findElement(By.cssSelector("[value='Edit']")).click();
-		WebElement outcome = programToModify.findElement(By.cssSelector("[ng-model='patientProgram.outcomeData']"));
-		new Select(outcome).selectByVisibleText(treatment.getTreatmentStatus());
-		programToModify.findElement(By.cssSelector("[value='Save']")).click();
-	}
+    public void editProgramAttributes(Program treatment, String registration, String facility) {
+        WebElement programToModify = findProgram(treatment);
+        programToModify.findElement(By.cssSelector("[value='Edit']")).click();
+        editProgram(registration, facility);
+        programToModify.findElement(By.cssSelector("[value='Save']")).click();
+    }
 
-	public ProgramManagementPage editProgram(String registration, String facility) {
-		facility_name.sendKeys(facility);
-		registration_id.sendKeys(registration);
-		return this;
-	}
+    public void endProgram(Program treatment) {
+        WebElement programToModify = findProgram(treatment);
+        programToModify.findElement(By.cssSelector("[value='Edit']")).click();
+        WebElement outcome = programToModify.findElement(By.cssSelector("[ng-model='patientProgram.outcomeData']"));
+        new Select(outcome).selectByVisibleText(treatment.getTreatmentStatus());
+        programToModify.findElement(By.cssSelector("[value='Save']")).click();
+    }
 
-	public Program transformTableRowToProgram(TableRow row, List<String> columnNames) {
-		String registration = row.getCell(columnNames.get(1)) + new Random().nextInt();
-		Program program = new Program(row.getCell(columnNames.get(0)), registration);
-		return program;
-	}
+    public ProgramManagementPage editProgram(String registration, String facility) {
+        facility_name.sendKeys(facility);
+        registration_id.sendKeys(registration);
+        return this;
+    }
 
-	public void deleteProgram(WebElement program){
-		program.findElement(By.cssSelector("[id=\"delete_btn\"]")).click();
-		program.findElement(By.cssSelector("[ng-model=\"patientProgram.voidReason\"]")).sendKeys("Deleting the program");
-		program.findElement(By.cssSelector("[value=\"Delete\"]")).click();
-		deleteProgramPopUp.findElement(By.cssSelector("[id=\"delete\"]")).click();
-	}
+    public Program transformTableRowToProgram(TableRow row, List<String> columnNames) {
+        String registration = row.getCell(columnNames.get(1)) + new Random().nextInt();
+        Program program = new Program(row.getCell(columnNames.get(0)), registration);
+        return program;
+    }
+
+    public void deleteProgram(WebElement program) {
+        program.findElement(By.cssSelector("[id=\"delete_btn\"]")).click();
+        program.findElement(By.cssSelector("[ng-model=\"patientProgram.voidReason\"]")).sendKeys("Deleting the program");
+        program.findElement(By.cssSelector("[value=\"Delete\"]")).click();
+
+        By popupLocator = By.className("delete-program-popup");
+        WebDriverWait wait = new WebDriverWait(this.driver, 15);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(popupLocator));
+        WebElement deletePopup = this.driver.findElement(popupLocator);
+        WebElement deleteButton = deletePopup.findElement(By.cssSelector("[id=\"delete\"]"));
+        deleteButton.click();
+    }
 
 }
--- bahmni-gauge-possible/specs/enrollPatientToProgram.spec
+++ bahmni-gauge-possible/specs/enrollPatientToProgram.spec
@@ -1,14 +1,9 @@
 Program Enrollemnt, Editing of Program Attributes and End the Program
 =====================================================================
-Created by dharmens on 7/5/16
-
-This is an executable specification file which follows markdown syntax.
-Every heading in this file denotes a scenario. Every bulleted point denotes a step.
-
+Tags: regression, sanity
 
 Create Patient and Enrol to a Program
 ---------------------------------------------------
-
 * On the login page
 * Login with username "BAHMNI_GAUGE_APP_USER" and password "BAHMNI_GAUGE_APP_PASSWORD" with location "BAHMNI_GAUGE_APP_LOCATION"
 * Create the following patient using api
@@ -20,17 +15,17 @@ Create Patient and Enrol to a Program
 * Select existing patient from patient listing page under tab "All"
 * Register the patient to following program
     |TB Register|Date Of Registration|Program State|
-    |ANC|01/01/2015|ANC program State 1|
-* Ensure that the patient is registered to "ANC"
+    |ANC Program|01/01/2015|ANC program State 1|
+* Ensure that the patient is registered to "ANC Program"
 
 Delete a Program
 -----------------------------
 * Navigate to dashboard
 * Click on programs app
 * Select existing patient from patient listing page under tab "All"
-* Ensure that the patient is registered to "ANC"
-* Unenroll patient from "ANC" program
-* Ensure that the patient is not registered to "ANC"
+* Ensure that the patient is registered to "ANC Program"
+* Unenroll patient from "ANC Program" program
+* Ensure that the patient is not registered to "ANC Program"
 
 //End the Program Enrolled for Patient
 //------------------------------------
