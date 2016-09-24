e96a2bf18160e454ab4f5eebae9d1446a671de4c
SwathiVarkala <swathivarkala@gmail.com>
2016-09-24 18:49:45 +0530

    Swathi | #000 | Added wait condition on adding a template in Obs Tab

--- bahmni-gauge-commons/src/test/java/org/bahmni/gauge/common/specs/ObservationSpec.java
+++ bahmni-gauge-commons/src/test/java/org/bahmni/gauge/common/specs/ObservationSpec.java
@@ -3,15 +3,12 @@ package org.bahmni.gauge.common.specs;
 import com.thoughtworks.gauge.BeforeClassSteps;
 import com.thoughtworks.gauge.Step;
 import com.thoughtworks.gauge.Table;
-import com.thoughtworks.gauge.TableRow;
 import org.bahmni.gauge.common.BahmniPage;
 import org.bahmni.gauge.common.DriverFactory;
 import org.bahmni.gauge.common.PageFactory;
-import org.bahmni.gauge.common.TestSpecException;
 import org.bahmni.gauge.common.clinical.DashboardPage;
 import org.bahmni.gauge.common.clinical.ObservationsPage;
 import org.bahmni.gauge.common.clinical.domain.DrugOrder;
-import org.bahmni.gauge.common.clinical.domain.ObservationForm;
 import org.bahmni.gauge.common.program.domain.PatientProgram;
 import org.bahmni.gauge.common.registration.domain.Patient;
 import org.bahmni.gauge.rest.BahmniRestClient;
@@ -40,6 +37,7 @@ public class ObservationSpec extends BahmniPage {
         ObservationsPage observationsPage = PageFactory.getObservationsPage();
         template = template.replace(" ", "_");
         observationsPage.selectTemplate(template);
+        waitForAppReady();
     }
 
     @Step("Create a <formName> form with following data <table>")
