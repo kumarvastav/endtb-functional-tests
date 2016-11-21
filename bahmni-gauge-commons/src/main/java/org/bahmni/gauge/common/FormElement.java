44c5e229cd87d493181a01c95a143f82e4686707
prabhashsingh <singhp@thoughtworks.com>
2016-11-21 16:10:07 +0530

    added proper imports

--- bahmni-gauge-commons/src/main/java/org/bahmni/gauge/common/FormElement.java
+++ bahmni-gauge-commons/src/main/java/org/bahmni/gauge/common/FormElement.java
@@ -1,6 +1,10 @@
 package org.bahmni.gauge.common;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
