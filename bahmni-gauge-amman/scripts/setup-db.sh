870c5f6c06362b2d17da274622cac51f0664284e
swarupkm <swarupmahapatra1@gmail.com>
2016-11-23 17:02:24 +0530

    Swarup | Amman, changing db setup script

--- bahmni-gauge-amman/scripts/setup-db.sh
+++ bahmni-gauge-amman/scripts/setup-db.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-ssh $ENVIRONMENT_USERNAME@$BAHMNI_GAUGE_APP_HOSTNAME "wget https://raw.githubusercontent.com/bahmni-msf/amman-config/master/dbdump/mysql_dump.sql && mysql -u $MYSQL_DB_USERNAME -p$MYSQL_DB_PASSWORD openmrs < mysql_dump.sql && rm mysql_dump.sql"
+ssh $ENVIRONMENT_USERNAME@$BAHMNI_GAUGE_APP_HOSTNAME "wget https://raw.githubusercontent.com/Bahmni/bahmni-scripts/master/deletePatientData/deletePatientDataForOpenMRS.sql && mysql -u $MYSQL_DB_USERNAME -p$MYSQL_DB_PASSWORD openmrs < deletePatientDataForOpenMRS.sql && rm deletePatientDataForOpenMRS.sql"
