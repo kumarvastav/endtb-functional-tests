0db08c7c08138b1c687e9321acf0abf053978b41
yaeli <yaeli@thoughtworks.com>
2017-03-09 15:41:36 +0800

    Yae & George | #3297 | publish the form and verify system behaviors in function test 1

--- bahmni-gauge-commons/src/main/java/org/bahmni/gauge/common/formBuilder/FormDetailPage.java
+++ bahmni-gauge-commons/src/main/java/org/bahmni/gauge/common/formBuilder/FormDetailPage.java
@@ -13,96 +13,99 @@ public class FormDetailPage extends BahmniPage {
     @FindBy(how = How.CSS, using = "button")
     public WebElement editButton;
 
-	@FindBy(how = How.CSS, using = ".button")
-	public WebElement okButton;
+    @FindBy(how = How.CSS, using = ".button")
+    public WebElement okButton;
 
-	@FindBy(how = How.CSS, using = ".breadcrumbs")
-	public  WebElement breadCrumbsGuider;
+    @FindBy(how = How.CSS, using = ".breadcrumbs")
+    public WebElement breadCrumbsGuider;
 
-	@FindBy(how = How.CSS, using = ".publish-button")
-    public  WebElement publishButton;
+    @FindBy(how = How.CSS, using = ".publish-button")
+    public WebElement publishButton;
 
     @FindBy(how = How.CSS, using = ".header-title")
-    public  WebElement formHeader;
+    public WebElement formHeader;
 
-	@FindBy(how = How.CSS, using = ".grid")
-	public  WebElement canvas;
+    @FindBy(how = How.CSS, using = ".grid")
+    public WebElement canvas;
 
-	@FindBy(how = How.CSS, using = ".save-button")
-	public  WebElement saveButton;
+    @FindBy(how = How.CSS, using = ".save-button")
+    public WebElement saveButton;
 
-	@FindBy(how = How.CSS, using = ".form-properties")
-	public  WebElement propertiesBody;
+    @FindBy(how = How.CSS, using = ".form-properties")
+    public WebElement propertiesBody;
 
-	public void clickOnEdit() {
-		editButton.click();
-	}
+    public void clickOnEdit() {
+        editButton.click();
+    }
 
-	public void clickOnOK() { okButton.click(); }
+    public void clickOnOK() {
+        okButton.click();
+    }
 
-	public void clickOnFormBuilder() {
-		breadCrumbsGuider.findElements(By.cssSelector("a")).get(1).click();
-	}
+    public void clickOnFormBuilder() {
+        breadCrumbsGuider.findElements(By.cssSelector("a")).get(1).click();
+    }
 
     public void clickOnPublish() {
-	    publishButton.click();
+        publishButton.click();
     }
 
     public String getFormInfo() {
         return formHeader.getText();
     }
 
-	public List<WebElement> getCanvasBodyLabelList() {
-		return canvas.findElements(By.cssSelector("label"));
-	}
+    public List<WebElement> getCanvasBodyLabelList() {
+        return canvas.findElements(By.cssSelector("label"));
+    }
 
-	public void clickOnSave() {
-		saveButton.click();
-	}
-	public void clickOnControl(WebElement control) {
-		control.click();
-	}
+    public void clickOnSave() {
+        saveButton.click();
+    }
 
-	public void clickOnProperty(String propertyType) {
-		List<WebElement> allPropertyType = propertiesBody.findElements(By.cssSelector("label"));
-		List<WebElement> propertyCheckBox = propertiesBody.findElements(By.cssSelector("input"));
+    public void clickOnControl(WebElement control) {
+        control.click();
+    }
 
-		for (int i = 0; i < allPropertyType.size(); i++) {
-			if (allPropertyType.get(i).getText().equals(propertyType)) {
-				propertyCheckBox.get(i).click();
-			}
-		}
-	}
+    public void clickOnProperty(String propertyType) {
+        List<WebElement> allPropertyType = propertiesBody.findElements(By.cssSelector("label"));
+        List<WebElement> propertyCheckBox = propertiesBody.findElements(By.cssSelector("input"));
+
+        for (int i = 0; i < allPropertyType.size(); i++) {
+            if (allPropertyType.get(i).getText().equals(propertyType)) {
+                propertyCheckBox.get(i).click();
+            }
+        }
+    }
 
 
-	public WebElement getEditButton() {
-		return editButton;
-	}
+    public WebElement getEditButton() {
+        return editButton;
+    }
 
-	public void changeName(String labelName, String name) {
-		List<WebElement> labelList = driver.findElements(By.tagName("label"));
-		for (WebElement label : labelList) {
-			if(label.getText().equals(labelName)){
-				WebElement parentElement = label.findElement(By.xpath("../.."));
+    public void changeName(String labelName, String name) {
+        List<WebElement> labelList = driver.findElements(By.tagName("label"));
+        for (WebElement label : labelList) {
+            if (label.getText().equals(labelName)) {
+                WebElement parentElement = label.findElement(By.xpath("../.."));
 
-				doubleClickOnLable(label);
+                doubleClickOnLable(label);
 
-				WebElement childElement = parentElement.findElement(By.cssSelector(".form-builder-label"));
+                WebElement childElement = parentElement.findElement(By.cssSelector(".form-builder-label"));
 
-				childElement.clear();
-				childElement.sendKeys(name);
+                childElement.clear();
+                childElement.sendKeys(name);
 
-				canvas.click();
-			}
-		}
-	}
+                canvas.click();
+            }
+        }
+    }
 
-	public void doubleClickOnLable(WebElement label) {
-		Actions action = new Actions(driver).doubleClick(label);
-		action.build().perform();
-	}
+    public void doubleClickOnLable(WebElement label) {
+        Actions action = new Actions(driver).doubleClick(label);
+        action.build().perform();
+    }
 
-	public boolean isPropertyChecked(String propertyType) {
+    public boolean isPropertyChecked(String propertyType) {
         List<WebElement> allPropertyType = propertiesBody.findElements(By.cssSelector("label"));
         List<WebElement> propertyCheckBox = propertiesBody.findElements(By.cssSelector("input"));
 
@@ -115,11 +118,11 @@ public class FormDetailPage extends BahmniPage {
         return false;
     }
 
-	public List<WebElement> getAllButton() {
-		return driver.findElement(By.cssSelector(".breadcrumb-wrap")).findElements(By.cssSelector("button"));
-	}
+    public List<WebElement> getAllButton() {
+        return driver.findElement(By.cssSelector(".breadcrumb-wrap")).findElements(By.cssSelector("button"));
+    }
 
-	public WebElement findCanvasTitle() {
-		return driver.findElement(By.cssSelector(".canvas-title"));
-	}
+    public WebElement findCanvasTitle() {
+        return driver.findElement(By.cssSelector(".canvas-title"));
+    }
 }
