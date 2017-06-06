d95b64dc4f4583c94762f9d7225532bcbdaf4806
kumarvastav <kumar.vastav@gmail.com>
2017-06-06 11:16:49 +0530

    Modifying the locator for status
    Modifying the locator for status

--- bahmni-gauge-commons/src/main/java/org/bahmni/gauge/common/clinical/DiagnosisPage.java
+++ bahmni-gauge-commons/src/main/java/org/bahmni/gauge/common/clinical/DiagnosisPage.java
@@ -109,7 +109,7 @@ public class DiagnosisPage extends BahmniPage {
                     }
                     if(diagnosis.getStatus().equalsIgnoreCase("inactive")){
                         if(!row.findElement(bStatus).getAttribute("class").contains("active"))
-                            row.findElement(bStatus).click();
+                            row.findElement(By.cssSelector(".status button")).click(); //bStatus
                     }
                 }
 
